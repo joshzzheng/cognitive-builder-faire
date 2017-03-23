e9ea93aeeb219df1511eff015574d47bf03776f9
Josh Zheng <denial@gmail.com>
2017-03-23 00:02:34 -0700

    initial commit, speech

---
+++ python/speech/stt.py
@@ -0,0 +1,53 @@
+import os
+import json
+from os.path import join, dirname
+from dotenv import load_dotenv
+from watson_developer_cloud import SpeechToTextV1 as SpeechToText
+from watson_developer_cloud import AlchemyLanguageV1 as AlchemyLanguage
+
+from recorder.recorder import Recorder
+
+def transcribe_audio(path_to_audio_file):
+    username = os.environ.get("BLUEMIX_USERNAME")
+    password = os.environ.get("BLUEMIX_PASSWORD")
+    speech_to_text = SpeechToText(username=username,
+                                  password=password)
+
+    with open(join(dirname(__file__), path_to_audio_file), 'rb') as audio_file:
+        return speech_to_text.recognize(audio_file,
+            content_type='audio/wav')
+
+def get_text_sentiment(text):
+    alchemy_api_key = os.environ.get("ALCHEMY_API_KEY")
+    
+    alchemy_language = AlchemyLanguage(api_key=alchemy_api_key)
+    result = alchemy_language.sentiment(text=text)
+    if result['docSentiment']['type'] == 'neutral':
+        return 'netural', 0
+    return result['docSentiment']['type'], result['docSentiment']['score']
+
+def main():
+    recorder = Recorder("speech.wav")
+
+    print("Please say something nice into the microphone\n")
+    recorder.record_to_file()
+
+    print("Transcribing audio....\n")
+    result = transcribe_audio('speech.wav')
+    
+    text = result['results'][0]['alternatives'][0]['transcript']
+    print("Text: " + text + "\n")
+    
+    sentiment, score = get_text_sentiment(text)
+    print(sentiment, score)  
+
+if __name__ == '__main__':
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    try:
+        main()
+    except:
+        print("IOError detected, restarting...")
+        main()
+
+
